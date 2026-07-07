375e431fa16fb1293c4cdf0e9d97de0ca41d597d
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-07 23:37:09 +0500

    newsletter section is complete

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Features from "../components/Home/Features"
 import HomeCategories from "../components/Home/HomeCategories"
 import PopularProducts from "../components/Home/PopularProducts"
 import AppPromoBanner from "../components/Home/AppPromoBanner"
+import Newsletter from "../components/Home/Newsletter"
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <HomeCategories />
       <PopularProducts />
       <AppPromoBanner />
+      <Newsletter/>
       
     </div>
   )
