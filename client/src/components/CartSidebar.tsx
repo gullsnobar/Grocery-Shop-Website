e58f2efc02c158804a113830cba6e2c0034361f0
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-11 23:16:14 +0500

    working on cardsidebar

---
+++ client/src/components/CartSidebar.tsx
@@ -0,0 +1,21 @@
+
+
+const CartSidebar = () => {
+  return (
+  <>
+    <div className="fixed inset-0 bg-black/40 z-50 transition-opacity">
+
+    { /* Cart content would go here */ }
+
+    <div className="fixed right-0 top-0 h-full w-full max-w-md bg-white shadow-2xl flex flex-col animate-slide-in-right">
+
+    </div>
+    
+    
+    </div>
+  
+  </>
+  )
+}
+
+export default CartSidebar
